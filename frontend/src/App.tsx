f6edada9a8a571323ab9b909f1e718f20d9f4b6e
developerchetram <patelchetram49@gmail.com>
2025-10-08 15:26:28 +0530

    red red errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/Navbar';
-import MainRoutes from './routes/MainRoutes';
-import Footer from './components/Footer';
-import './App.css';
 
-const App: React.FC = () => {
+const App = () => {
   return (
-    <Router>
-      <div className="app-container">
-        <ToastContainer position="bottom-right" autoClose={3000} hideProgressBar={false} />
-        <Navbar />
-        <main className="main-content">
-          <MainRoutes />
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
--- frontend/src/config/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: '/api' });
-
-// You can also add interceptors here if needed, for example, to attach auth tokens.
-
-export default API;
